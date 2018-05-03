1f7971176850f33c53700916097c6c62baf7a723
MarcoCab <MarcoCaballero@users.noreply.github.com>
2018-05-03 19:33:03 +0200

    FIrst commit
    Adding first classes.

---
+++ src/main/java/eacs/tfd/solver/Constant.java
@@ -0,0 +1,5 @@
+package eacs.tfd.solver;
+
+public class Constant {
+
+}
---
+++ src/test/java/ConstantTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class ConstantTest {
+	
+	@Test
+	public void clone_constant_test() {
+		
+	}
+}
